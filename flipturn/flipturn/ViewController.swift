443bd810f0b7921418fd458f50cba23f61bb497e
Patrick Luddy <pluddy@outlook.com>
2015-11-28 20:21:39 -0600

    initial commit

---
+++ flipturn/flipturn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flipturn
+//
+//  Created by Patrick Luddy on 11/28/15.
+//  Copyright © 2015 pluddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
